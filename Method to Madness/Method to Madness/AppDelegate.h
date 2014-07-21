cc7c73f9785096674e6c7ef3b81b59ff492c72ae
Brian Tracy <silicode.team@gmail.com>
2014-07-21 11:09:20 -0700

    App Delegate - Brian Tracy

---
+++ Method to Madness/Method to Madness/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Method to Madness
+//
+//  Created by Brian Tracy on 7/20/14.
+//  Copyright (c) 2014 Silicode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+
+// hello matthew this is a test for our git
+
+@end
